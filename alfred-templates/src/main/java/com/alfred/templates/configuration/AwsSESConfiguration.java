53ce5fdd4bd7457f6d3130b18530b991112b71bb
Renan Palmeira <renanpalmeira1@hotmail.com>
2018-01-14 00:13:45 -0200

    Initial AwsSESConfiguration in com.alfred.templates.configuration

---
+++ alfred-templates/src/main/java/com/alfred/templates/configuration/AwsSESConfiguration.java
@@ -0,0 +1,34 @@
+package com.alfred.templates.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+
+@Configuration
+class AwsSESConfiguration {
+
+    @Value("${com.alfred.region}")
+    private String REGION;
+
+    @Value("${com.alfred.accessKey}")
+    private String ACCESSKEY;
+
+    @Value("${com.alfred.secretKey}")
+    private String SECRETKEY;
+
+    @Bean
+    AmazonSimpleEmailService ses() {
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(ACCESSKEY, SECRETKEY);
+
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+              .withCredentials(new AWSStaticCredentialsProvider(credentials))
+              // For a complete list, see http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html
+              .withRegion(REGION)
+              .build();
+    }
+}
