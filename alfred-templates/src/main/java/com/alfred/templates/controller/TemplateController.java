6da6dbd16518570384da9f33e7038dec83007da8
Renan Palmeira <renanpalmeira1@hotmail.com>
2018-01-18 00:14:53 -0200

    Initial /template endpoint

--- alfred-templates/src/main/java/com/alfred/templates/controller/TemplateController.java
+++ alfred-templates/src/main/java/com/alfred/templates/controller/TemplateController.java
@@ -1,7 +1,23 @@
 package com.alfred.templates.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.ListTemplatesRequest;
+import com.amazonaws.services.simpleemail.model.ListTemplatesResult;
 
 @RestController
-class TemplateController {
+@RequestMapping(value = "/template/")
+public class TemplateController {
+
+    @Autowired
+    private AmazonSimpleEmailService ses;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ListTemplatesResult listTemplates() {
+        return ses.listTemplates(new ListTemplatesRequest());
+    }
+
 }
