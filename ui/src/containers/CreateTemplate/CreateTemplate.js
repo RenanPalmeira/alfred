7548d254b11e705b58259d3c75679782bc04a258
Renan Palmeira <renanpalmeira1@hotmail.com>
2018-03-02 08:51:07 -0300

    Update mode of codemirror

--- ui/src/containers/CreateTemplate/CreateTemplate.js
+++ ui/src/containers/CreateTemplate/CreateTemplate.js
@@ -2,10 +2,7 @@ import React from "react";
 import {UnControlled as CodeMirror} from 'react-codemirror2'
 import '../../../node_modules/codemirror/lib/codemirror.css'
 import '../../../node_modules/@atlaskit/reduced-ui-pack/dist/bundle.css'
-import '../../../node_modules/codemirror/mode/htmlembedded/htmlembedded'
-import '../../../node_modules/codemirror/mode/htmlmixed/htmlmixed'
-import '../../../node_modules/codemirror/mode/javascript/javascript'
-import '../../../node_modules/codemirror/mode/xml/xml'
+import '../../../node_modules/codemirror/mode/jinja2/jinja2'
 import PropTypes from "prop-types";
 
 export default class CreateTemplate extends React.Component {
@@ -48,9 +45,9 @@ export default class CreateTemplate extends React.Component {
                     <div className="ak-field-group">
                         <label>Template</label>
                         <CodeMirror
-                            value='<h1>Here your pretty HTML to this template</h1>'
+                            value='<h1>Here your pretty HTML to this {{template}}</h1>'
                             options={{
-                                mode: 'xml',
+                                mode: 'jinja2',
                                 lineNumbers: true
                             }}
                             onChange={(editor, data, value) => {
