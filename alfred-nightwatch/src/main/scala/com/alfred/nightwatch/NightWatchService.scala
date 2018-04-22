335c6e59ba8aa6eb32aa2d1455bd6a576297c0d9
Renan Palmeira <renanpalmeira1@hotmail.com>
2018-04-21 22:11:44 -0300

    Trying remove blocking execution

--- alfred-nightwatch/src/main/scala/com/alfred/nightwatch/NightWatchService.scala
+++ alfred-nightwatch/src/main/scala/com/alfred/nightwatch/NightWatchService.scala
@@ -5,7 +5,7 @@ import scala.util.Try
 import featherbed.request.ErrorResponse
 import com.twitter.finagle.FailedFastException
 import com.twitter.logging.Logger
-import com.twitter.util.{ Return, Throw }
+import com.twitter.util.{ Future, Return, Throw }
 import com.twitter.finagle.http.Response
 
 import com.alfred.nightwatch.configuration.Configuration.RoutesConfig
@@ -16,24 +16,31 @@ class NightWatchService {
 
   private val services: List[String] = RoutesConfig.services
 
+  def fetchService(service: String): Future[Response] = {
+    val link = new URL(service)
+    val client = new featherbed.Client(link)
+    client
+      .get(link.getPath)
+      .withHeaders("User-Agent" -> "nightwatch-bot")
+      .send[Response]()
+      .onSuccess {
+        response => log.info(s"${link.getHost} - ${response.statusCode}")
+      }
+      .onFailure {
+        case ErrorResponse(request, response) =>
+          log.info(s"${link.getHost} - ${response.statusCode}")
+
+        case e: Throwable => {
+          log.error(s"${link.getHost} - ${e.getMessage}")
+          NightWatchBot.send(s"${link.getHost} - ***DOWN***", true)
+        }
+      }
+  }
+
   def ping(): Unit = {
     for {
       service <- services
-    } yield for {
-      link <- Try(new URL(service))
-      client <- Try(new featherbed.Client(link))
-      request <- Try(client.get(link.getPath).withHeaders("User-Agent" -> "nightwatch-bot").send[Response]())
-    } yield request.respond {
-
-      case Return(response) =>
-        log.info(s"${link.getHost} - ${response.statusCode}")
-
-      case Throw(ErrorResponse(request, response)) =>
-        log.info(s"${link.getHost} - ${response.statusCode}")
-
-      case Throw(e) =>
-        NightWatchBot.send(s"${link.getHost} - ***DOWN***", true)
-    }
+    } yield fetchService(service)
   }
 
 }
